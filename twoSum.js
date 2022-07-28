ee23cd9945cef57a741908e45edf702ccdf85bbb
Mohammad Tariq <fk68790@gmail.com>
2022-07-28 11:48:32 +0530

    HashMap method added

--- twoSum.js
+++ twoSum.js
@@ -1,19 +1,36 @@
+
+// Brute force
+// Complexity O(n^2) => nested loops
+
+// function twoSum(nums, target){
+//     let result = [];
+//
+//     for(let i = 0; i < nums.length - 1; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 result.push(nums.indexOf(nums[i]),nums.indexOf(nums[j]))
+//                 return result
+//             }
+//         }
+//     }
+// }
+
+//HashMap => Complexity O(n)
 function twoSum(nums, target){
-    let result = [];
-
-    for(let i = 0; i < nums.length - 1; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                result.push(nums.indexOf(nums[i]),nums.indexOf(nums[j]))
-                return result
-            }
-        }
-    }
-
+	let map = {};
+	let result = [];
+	for(let i = 0; i < nums.length; i++){
+		let diff = target - nums[i];
+		if(map[diff] >= 0){
+			result.push(map[diff],i);
+			return result;
+		}else{
+			map[nums[i]] = i;
+		}
+	}
 }
-
-let nums = [2,7,11,15];
-let target = 9;
+let nums = [3,3];
+let target = 6;
 
 let result = twoSum(nums, target);
 
